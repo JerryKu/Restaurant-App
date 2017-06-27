c3f86b812403d35ae268b4239d7c3324ccf08b8b
Jerry Ku <jerryku1220@gmail.com>
2017-06-27 05:58:00 +0000

    created database, added some RESTful routes, added show page by id

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/restaurants");
+
+//SCHEMA Setup
+var restaurantSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+// Restaurant.create(
+//     {
+//         name: "McDonald's",
+//         image: "http://is2.mzstatic.com/image/thumb/Purple127/v4/8a/4d/a0/8a4da0bc-2f59-9f66-4923-05fd8e4959e6/source/1200x630bb.jpg",
+//         description: "This is just a McDonald's"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log(campground)
+//         }
+//     });
 
 var restaurants = [
     {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
@@ -19,21 +44,43 @@ app.get('/', function(req, res){
 });
 
 app.get("/restaurants", function(req,res){
-    res.render("restaurants",{restaurants:restaurants});
+    Restaurant.find({}, function(err, allRestaurants){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {restaurants:allRestaurants});
+        }
+    })
 });
 
 app.post("/restaurants", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newRestaurant = {name: name, image: image};
-    restaurants.push(newRestaurant);
-    res.redirect("/restaurants");
+    Restaurant.create(newRestaurant, function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/restaurants");
+        }
+    })
 });
 
+//NEW - show form to create new
 app.get("/restaurants/new", function(req,res){
     res.render("new.ejs");
 });
 
+//SHOW - shows more info about one campground
+app.get("/restaurants/:id", function(req,res){
+    Restaurant.findById(req.params.id, function(err, foundRestaurant){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show", {restaurant: foundRestaurant});
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Yelp Camp Server Started.");
